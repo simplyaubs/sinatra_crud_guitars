f1322847e7419341a32006cbddab32ceef2633d2
Aubrey Howell <simplyaubs@gmail.com>
2014-04-28 07:43:19 -0600

    user can update the information of a guitar

--- application.rb
+++ application.rb
@@ -23,4 +23,16 @@ class Application < Sinatra::Application
     DB[:guitars].insert(maker: params[:maker], type: params[:type], color: params[:color])
     redirect '/'
   end
+
+  get '/guitars/:id' do
+    guitar_id = params[:id]
+    erb :show, locals: { single_guitar: DB[:guitars][id: guitar_id] }
+  end
+
+  put '/guitars/:id' do
+    guitar_id = params[:id]
+    DB[:guitars].where(id: guitar_id).update(maker: params[:maker], type: params[:type], color: params[:color])
+    redirect "/guitars/#{guitar_id}"
+  end
+
 end
--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -14,5 +14,16 @@ feature 'Homepage' do
     expect(page).to have_content 'Taylor'
   end
 
-  
+  scenario 'user can update a guitar' do
+    visit '/guitars/new'
+    fill_in 'maker', with: 'Taylor'
+    fill_in 'type', with: '314ce'
+    fill_in 'color', with: 'mahogany'
+    click_on 'Create Guitar'
+    click_on 'Taylor'
+    fill_in 'maker', with: 'Martin'
+    click_on 'Update Guitar'
+    expect(page).to have_content 'Martin'
+    expect(page).to_not have_content 'Taylor'
+  end
 end
